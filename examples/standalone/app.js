f2561c857f911ff94e6c17ea196542af17b3cdfb
Caridy Patino <caridy@gmail.com>
2013-02-27 12:36:41 -0500

    adding docs

--- examples/standalone/app.js
+++ examples/standalone/app.js
@@ -16,28 +16,41 @@ yui({
 }, __dirname + '/../../node_modules/yui');
 
 app.configure('development', function () {
-    yui.debug({
+    // when using `yui.debug()` you will get debug,
+    // filter and logLevel set accordingly
+    yui.debug();
+
+    // In development, we can get YUI from CDN, but
+    // passing some extra configuration, like `combine`
+    // to facilitate debugging.
+    yui.serveCoreFromCDN({
         combine: false
     });
 });
 
 app.configure('production', function () {
-    // normally, production is the default configuration
-});
-
-app.use(
-    yui.seed(['yui-base', 'loader-base', 'loader-yui3']),
+    // normally, production is the default configuration,
+    // but here is an example of forcing to use CDN
+    // for yui core modules with a custom root folder
     yui.serveCoreFromCDN({
         root: yui.version + "/build/"
-    })
-);
+    });
+});
+
+// by default, the seed will be just `yui-base`, but we can
+// extend the list by adding more modules to the seed constructor
+app.use(yui.seed(['yui-base', 'loader-base', 'loader-yui3']));
 
 // printing runtime information
 app.get('*', yui.expose(), function (req, res, next) {
     res.send({
+        // runtime configuration
         app: yui.config(),
+        // request specific configs
         res: res.locals.yui || null,
+        // blob built from static configuration
         yui_config: res.locals.yui_config,
+        // blob built from seed definition statically
         yui_seed: res.locals.yui_seed
     });
 });
