62fecf0b1fe567a5226d5604a02336aea73fe493
albertoc <albertoc@yahoo-inc.com>
2013-02-28 17:01:59 -0800

    Updated build config

--- scripts/posttest.sh
+++ scripts/posttest.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Running JSLint and YUIDocs"
 ./node_modules/.bin/jslint ./lib/*
-./node_modules/.bin/yuidoc --config ./yuidoc.json ./lib
+./node_modules/.bin/yuidoc --config ./conf/yuidoc.json ./lib
