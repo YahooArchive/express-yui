f4df60b2b7f395e0079661b170b601cd284e9c1d
Caridy Patino <caridy@gmail.com>
2013-05-02 15:20:20 -0400

    jslint <3

--- examples/locator-express/binders/index.js
+++ examples/locator-express/binders/index.js
@@ -1,4 +1,7 @@
-YUI.add('binder-index', function(Y) {
+/*jslint */
+/*global YUI, console*/
+YUI.add('binder-index', function (Y) {
+    "use strict";
     console.warn('if you see this in the server side console, then something is really wrong here!');
     Y.one('body').append('<p>binder is in place!</p>');
 }, '', {
